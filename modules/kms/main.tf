0f043478a2dbc25c639e5dc8541237d6a49beb43
eybolo <emanuelojeda10@gmail.com>
2026-05-18 01:01:31 -0300

    fix(kms): resolve syntax error in policy by combining statements using concat

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -7,16 +7,18 @@ resource "aws_kms_key" "this" {
   policy = jsonencode({
     Version = "2012-10-17"
     Id      = "key-default-1"
-    Statement = [
-      {
-        Sid    = "Enable IAM User Permissions"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+    Statement = concat(
+      [
+        {
+          Sid    = "Enable IAM User Permissions"
+          Effect = "Allow"
+          Principal = {
+            AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+          }
+          Action   = "kms:*"
+          Resource = "*"
         }
-        Action   = "kms:*"
-        Resource = "*"
-      },
+      ],
       [for service, config in var.service_principals : {
         Sid       = "AllowUsageTo-${replace(service, ".", "")}"
         Effect    = "Allow"
@@ -29,8 +31,9 @@ resource "aws_kms_key" "this" {
             "aws:SourceArn" = "arn:aws:${var.service_name}:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:*"
           }
         }
-      }]
-    ]
+        }
+      ]
+    )
   })
   tags = local.common_tags
 }
