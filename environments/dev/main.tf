409dc4121f493d19b2b67354c72600f08cbcd264
eybolo <emanuelojeda10@gmail.com>
2026-06-08 09:38:07 -0300

    feat(dev): add rds-aurora, alb and asg modules to dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -44,6 +44,9 @@ module "kms_s3" {
 module "kms_ebs" {
   source = "../../modules/kms"
 
+  # Configuracion General
+  environment  = var.environment
+
   service_principals = {
     "ec2.amazonaws.com" = {
       actions = [
@@ -55,5 +58,71 @@ module "kms_ebs" {
     }
   }
   service_name = "ebs"
-  environment  = var.environment
+}
+
+module "rds_aurora" {
+  source = "../../modules/rds-aurora"
+
+  # Configuracion General
+  environment           = var.environment
+
+  # Configuración de Red
+  vpc_id                = module.vpc.vpc_id
+  subnets_ids_data      = module.vpc.subnet_data_id
+  subnets_cidrs_private = module.vpc.subnet_private_cidr
+
+  # Seguridad y Cifrado
+  key_arn = module.kms_rds.key_arn
+
+  # Configuración del Motor
+  engine_version  = var.engine_version
+  database_name   = var.database_name
+  master_username = var.master_username
+
+  # Dimensionamiento del Cómputo
+  instance_count = var.instance_count
+  instance_class = var.instance_class
+}
+
+module "alb" {
+  source = "../../modules/alb"
+  
+  # Configuracion General
+  environment           = var.environment
+
+  # Configuración de Red
+  vpc_id                = module.vpc.vpc_id
+  subnets_ids_public    = module.vpc.subnet_public_id
+  subnets_cidrs_private = module.vpc.subnet_private_cidr
+
+  # Seguridad y Certificados
+  certificate_arn       = var.certificate_arn
+  access_logs_s3        = var.access_logs_s3
+}
+
+module "asg" {
+  source = "../../modules/asg"
+
+  # Configuracion General
+  environment           = var.environment
+
+  # Configuración de Red
+  vpc_id         = module.vpc.vpc_id
+  subnet_private = module.vpc.subnet_private_id
+
+  # Integración con ALB
+  arn_target_group      = module.alb.target_group_arn
+  security_group_id_alb = [module.alb.security_group_id]
+
+  # Integración con Aurora
+  security_group_id_aurora = [module.rds_aurora.security_group_id]
+
+  # Configuración de Cómputo
+  instance_type        = var.instance_type
+  instance_volume_size = var.instance_volume_size
+
+  # Configuración de Escalado
+  asg_desired_capacity = var.asg_desired_capacity
+  asg_min_size         = var.asg_min_size 
+  asg_max_size         = var.asg_max_size
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -27,3 +27,63 @@ variable "environment" {
   description = "Deployment environment (e.g., dev, staging, prod)"
   type        = string
 }
+
+variable "engine_version" {
+  type        = string
+  description = "The database engine version to use for the Aurora cluster (e.g., 3.05.2)."
+}
+
+variable "database_name" {
+  type        = string
+  description = "The name for the initial database to be created when the cluster is provisioned."
+}
+
+variable "master_username" {
+  type        = string
+  description = "The login username for the master administrative user of the cluster."
+}
+
+variable "instance_class" {
+  type        = string
+  description = " Instance class to use. For details on CPU and memory,"
+}
+
+variable "instance_count" {
+  type        = number
+  description = "instance number in the cluster"
+}
+
+variable "certificate_arn" {
+  description = "The ARN of the ACM SSL/TLS certificate used to secure the Load Balancer HTTPS listener."
+  type = string 
+}
+
+variable "access_logs_s3" {
+  type = string
+  description = "Access logs for buckets s3"
+}
+
+variable "instance_type" {
+  description = "Type instance ec2"
+  type        = string
+}
+
+variable "asg_desired_capacity" {
+  description = "The number of Amazon EC2 instances that should be running in the group."
+  type        = number
+}
+
+variable "asg_min_size" {
+  description = "The minimum size of the Auto Scaling Group."
+  type        = number
+}
+
+variable "asg_max_size" {
+  description = "The maximum size of the Auto Scaling Group."
+  type        = number
+}
+
+variable "instance_volume_size" {
+  description = "Volume size disk for instance ec2"
+  type        = number
+}
